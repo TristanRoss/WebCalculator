6112f909527f3c9b27e5fb42f3944bedbc9ea62d
Tristan Ross <tgr1998@live.com>
2019-05-07 00:26:54 -0700

    Finish code files

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 
 function add(x, y) {
-    return x + y;
+    return Number(x) + Number(y);
 }
 
 function subtract(x, y) {
@@ -35,17 +35,46 @@ const display = document.querySelector("#view");
 
 const buttons = document.querySelectorAll('button');
 
-//document.addEventListener('click', event => console.log(event.target.textContent));
+let firstDisplayValue = "";
+let secondDisplayValue = "";
+let operator = "";
+// document.addEventListener('click', event => console.log(event.target.textContent));
 
 buttons.forEach((button) => {
 
     button.addEventListener('click', () => {
         if (strDigits.includes(button.textContent)) {
-            if (display.textContent === "0") {
+            if (display.textContent === "0" || display.textContent === firstDisplayValue) {
                 display.textContent = button.textContent;
+            } else if (button.textContent === "0" && display.textContent === "0") {
+                firstDisplayValue = "0";
             } else {
             display.textContent += button.textContent;
             }
+        } else if(button.textContent === "=") {
+            secondDisplayValue = display.textContent;
+            if (secondDisplayValue === "0" && operator === "÷") {
+                alert("DON'T DIVIDE BY ZERO");
+                firstDisplayValue = "";
+                secondDisplayValue = "";
+                display.textContent = "0";
+                operator = "";
+            } else {
+                let num = operate(firstDisplayValue, secondDisplayValue, operator).toFixed(4);
+                if (num % 1 != 0) {
+                    display.textContent = num;
+                } else {
+                    display.textContent = parseInt(num);
+                }
+                operator = "";
+            }
+        } else if (button.textContent === "C") {
+            display.textContent = "0";
+            firstDisplayValue = "";
+            secondDisplayValue = "";
+        } else {
+            operator = button.textContent;
+            firstDisplayValue = display.textContent;
         }
     });
 });
